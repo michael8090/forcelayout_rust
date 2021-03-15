5796515f3ae1b47fd6ac5393e8db83a7e5d356d1
michael <michael8090@gmail.com>
2021-03-15 21:10:52 +0800

    update build shader

--- build_shaders.sh
+++ build_shaders.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-glslangValidator -V ./shaders/geometry.vert.glsl -o ./shaders/geometry.vert.spv
-glslangValidator -V ./shaders/geometry.frag.glsl -o ./shaders/geometry.frag.spv
-glslangValidator -V ./shaders/background.vert.glsl -o ./shaders/background.vert.spv
-glslangValidator -V ./shaders/background.frag.glsl -o ./shaders/background.frag.spv
-glslangValidator -V ./shaders/compute_repulsion.comp.glsl -o ./shaders/compute_repulsion.comp.spv
-glslangValidator -V ./shaders/compute_pull.comp.glsl -o ./shaders/compute_pull.comp.spv
-glslangValidator -V ./shaders/compute_position.comp.glsl -o ./shaders/compute_position.comp.spv
+
+for file in $(find . -type f -name "*.glsl"); do
+    glslangValidator -V ${file} -o ${file/glsl/spv};
+done
